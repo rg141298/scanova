46e6287b22ac6cd1559c63266e2a3bdf506c4965
goyalraman <raman@excellencetechnologies.in>
2021-02-02 21:53:13 +0530

    Added vuetify, created card and functionality to change text on click

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img v-if="false" alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld v-if="false" msg="Welcome to Your Vue.js App"/>
+    <Card/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Card
   }
 }
 </script>
